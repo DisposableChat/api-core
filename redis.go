448343f85a3d01364090d3b3f39e06d1b642a7ab
Jean Vides <videsrudas@gmail.com>
2023-01-08 12:51:21 -0500

    redis username support

--- redis.go
+++ redis.go
@@ -8,13 +8,19 @@ import (
 type Redis struct {
 	Host     string
 	Port     int16
+	Username  string
 	Password string
 	Client   *redis.Client
 }
 
 func (r *Redis) Init() (*redis.Client) {
+	if r.Host == "" || r.Port < 0 || r.Username == "" || r.Password == "" {
+		panic("REDIS_ENVIROMENT_VARIABLES not set up correctly")
+	}
+
 	r.Client = redis.NewClient(&redis.Options{
 		Addr: fmt.Sprintf("%s:%d", r.Host, r.Port),
+		Username: r.Username,
 		Password: r.Password,
 		DB: 0,
 	})
